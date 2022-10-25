7a014e17e894b1d08f0b96d9bcadaa4e93b12082
dukindroid <dukintosh@gmail.com>
2022-10-25 04:58:32 +0000

    Merge main of ecommerce-shopHealthy main-deployada

--- api/index.js
+++ api/index.js
@@ -1,11 +1,8 @@
 const { Sequelize } = require('sequelize');
 const server = require('./src/app.js');
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> 70a1947cc1e80c4f7895339109536c501dcfe02d
 const { conn } = require('./src/db');
 
 // Syncing all the models at once.
